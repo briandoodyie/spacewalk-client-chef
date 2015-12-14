f696782ca5d181b4e9d300b4ebc6a28d52b43323
briandoodyie <doody@hp.com>
2015-12-14 14:36:30 +0000

    Updating to version spacewalk 2.3 to match server cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['spacewalk']['pkg_source_path'] = Chef::Config[:file_cache_path]
-default['spacewalk']['rhel']['base_url'] = 'http://yum.spacewalkproject.org/2.2-client/RHEL'
-default['spacewalk']['rhel']['repo_version'] = '2.2-1'
+default['spacewalk']['rhel']['base_url'] = 'http://yum.spacewalkproject.org/2.3-client/RHEL'
+default['spacewalk']['rhel']['repo_version'] = '2.3-2'
 default['spacewalk']['enable_osad'] = false
 default['spacewalk']['enable_rhncfg'] = false
-default['spacewalk']['reg']['key'] = 'my-reg-key'
-default['spacewalk']['reg']['server'] = 'http://spacewalk.example.com'
+default['spacewalk']['reg']['key'] = '5fc6ed2f110b3f91c6b9b6d8e0261390'
+default['spacewalk']['reg']['server'] = 'www.spacewalk.com'
 default['spacewalk']['rhncfg']['actions']['run'] = false
